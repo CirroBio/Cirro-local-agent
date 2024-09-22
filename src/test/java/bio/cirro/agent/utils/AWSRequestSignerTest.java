54233e3f173b07d62ff6c3e1f9a47f41fc389a94
Nathan Thorpe <nathan@techstormpc.com>
2024-09-21 21:07:08 -0700

    aws request signer test

---
+++ src/test/java/bio/cirro/agent/utils/AWSRequestSignerTest.java
@@ -0,0 +1,47 @@
+package bio.cirro.agent.utils;
+
+import bio.cirro.agent.exception.AgentException;
+import io.micronaut.http.HttpRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AWSRequestSignerTest {
+    AWSRequestSigner awsRequestSigner;
+
+    @BeforeEach
+    void setUp() {
+        System.clearProperty("aws.accessKeyId");
+        System.clearProperty("aws.secretAccessKey");
+        awsRequestSigner = new AWSRequestSigner();
+    }
+
+    @Test
+    void testSign() {
+        var exampleKey = "EXAMPLE_KEY";
+        System.setProperty("aws.accessKeyId", exampleKey);
+        System.setProperty("aws.secretAccessKey", "test");
+
+        var request = HttpRequest
+                .GET("http://localhost:8080")
+                .body("");
+        var signedRequest = awsRequestSigner.signRequest(request);
+        var headers = signedRequest.getHeaders();
+        Assertions.assertTrue(headers.contains("X-Amz-Date"));
+        Assertions.assertTrue(headers.contains("Authorization"));
+        Assertions.assertTrue(headers.contains("x-amz-content-sha256"));
+
+        var authorization = headers.get("Authorization");
+        Assertions.assertTrue(authorization.startsWith("AWS4-HMAC"));
+        Assertions.assertTrue(authorization.contains(exampleKey));
+        Assertions.assertTrue(authorization.contains("execute-api"));
+    }
+
+    @Test
+    void testThrowsNoCredentials() {
+        var request = HttpRequest
+                .GET("http://localhost:8080")
+                .body("");
+        Assertions.assertThrows(AgentException.class, () -> awsRequestSigner.signRequest(request));
+    }
+}
