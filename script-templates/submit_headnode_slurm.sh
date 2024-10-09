a260380b8e64942b17da19836d93aa3b7bf06ac7
Sam Minot <sminot@gmail.com>
2024-10-09 11:18:42 -0700

    Script to submit the headnode job to the cluster

---
+++ script-templates/submit_headnode_slurm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script is used to submit the headnode job to the cluster.
+# It will be run by the agent process itself, and should take very little time to execute.
+
+# Environment variables required:
+
+# - PROCESS_DIR: The directory which will be used for the headnode execution (absolute path)
+# - PROCESS_GUID: The unique identifier for the process
+# - HEADNODE_ACCOUNTING: The accounting string to use for the headnode job
+# - HEADNODE_PARTITION: The partition to use for the headnode job
+# - HEADNODE_CPUS: The number of CPUs to use for the headnode job
+# - HEADNODE_MEM: The amount of memory to use for the headnode job (e.g. 4G)
+# - HEADNODE_PRIORITY: The priority to use for the headnode job
+
+sbatch \
+    run_headnode.sh \
+    --chdir="${PROCESS_DIR}" \
+    --error="${PROCESS_DIR}/process.err" \
+    --output="${PROCESS_DIR}/process.out" \
+    --job-name="${PROCESS_GUID}" \
+    --account="${HEADNODE_ACCOUNTING}" \
+    --partition="${HEADNODE_PARTITION}" \
+    --cpus-per-task=${HEADNODE_CPUS} \
+    --mem="${HEADNODE_MEM}" \
+    --priority="${HEADNODE_PRIORITY}"
