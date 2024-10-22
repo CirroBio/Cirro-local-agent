6ccf063012b0feeb0a320152a3bb30934b926712
Nathan Thorpe <nathan@techstormpc.com>
2024-10-22 16:05:55 -0700

    fix credential helper env

--- src/main/resources/credentials-helper.sh
+++ src/main/resources/credentials-helper.sh
@@ -2,21 +2,21 @@
 # https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-sourcing-external.html
 
 # Check if environment variables are set
-if [ -z "$CIRRO_AGENT_ENDPOINT" ]; then
-  echo "CIRRO_AGENT_ENDPOINT is not set"
+if [ -z "$AGENT_ENDPOINT" ]; then
+  echo "AGENT_ENDPOINT is not set"
   exit 1
 fi
 
-if [ -z "$CIRRO_EXECUTION_ID" ]; then
-  echo "CIRRO_EXECUTION_ID is not set"
+if [ -z "$AGENT_EXECUTION_ID" ]; then
+  echo "AGENT_EXECUTION_ID is not set"
   exit 1
 fi
 
-if [ -z "$CIRRO_AGENT_TOKEN" ]; then
-  echo "CIRRO_AGENT_TOKEN is not set"
+if [ -z "$AGENT_TOKEN" ]; then
+  echo "AGENT_TOKEN is not set"
   exit 1
 fi
 
-curl "${CIRRO_AGENT_ENDPOINT}/executions/${CIRRO_EXECUTION_ID}/s3-token" \
+curl "${AGENT_ENDPOINT}/executions/${AGENT_EXECUTION_ID}/s3-token" \
   -X POST \
-  -H "Authorization: Bearer ${CIRRO_AGENT_TOKEN}"
+  -H "Authorization: Bearer ${AGENT_TOKEN}"
