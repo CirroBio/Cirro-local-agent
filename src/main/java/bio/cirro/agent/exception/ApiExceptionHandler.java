f94de197ccf4bd0d66339857e5bbe29febe1987e
Nathan Thorpe <nathan@techstormpc.com>
2024-11-07 16:02:58 -0800

    log error when API returns internal server error

--- src/main/java/bio/cirro/agent/exception/ApiExceptionHandler.java
+++ src/main/java/bio/cirro/agent/exception/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles exceptions thrown by the API.
@@ -15,16 +16,20 @@ import jakarta.inject.Singleton;
 @Produces
 @Singleton
 @Requires(classes = {ExceptionHandler.class})
+@Slf4j
 public class ApiExceptionHandler implements ExceptionHandler<RuntimeException, HttpResponse<?>> {
 
     @Override
     public HttpResponse<?> handle(HttpRequest request, RuntimeException exception) {
-        var status = switch (exception ) {
+        var status = switch (exception) {
             case SecurityException ignore -> HttpStatus.FORBIDDEN;
             case IllegalArgumentException ignore -> HttpStatus.BAD_REQUEST;
             case IllegalStateException ignore -> HttpStatus.BAD_REQUEST;
             default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
+        if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
+            log.error("Unhandled exception", exception);
+        }
         return HttpResponse.status(status).body(new ErrorMessage(exception.getMessage()));
     }
 
